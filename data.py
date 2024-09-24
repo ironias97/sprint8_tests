e7de9b241e052a0dd70a00943f7e5b3e7f4e8d57
ironias97 <josue_sgg@hotmail.com>
2024-09-23 19:07:13 -0600

    eliminar archivos de seguimiento para ignorarlos

--- data.py
+++
@@ -1,5 +0,0 @@
-email = "josue@hotmail.com"
-password = "Josue123"
-around_link = "https://around-v1.nm.tripleten-services.com/signin?lng=es"
-avatar_url = "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/qa-sprint-7/avatarSelenium.png"
-picture_url = "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/qa-sprint-7/photoSelenium.jpg"
--- find_elements_ejercicio1.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-import data
-
-driver = webdriver.Chrome()
-driver.get(data.around_link)
-driver.find_element(By.CSS_SELECTOR,'.auth-form__title')
-
-driver.quit()
